7cb5376247a3b5c2b5e46435364f2afbbc4b367b
Ryan Phillips <ryan.phillips@rackspace.com>
2015-06-30 08:23:41 -0500

    bump to 2.1.4 lit

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 echo off
 
 set LUVI_VERSION=release
-set LIT_VERSION=2.1.2
+set LIT_VERSION=2.1.4
 set RMA_VERSION=luvi-up
 
 set LIT_URL="https://lit.luvit.io/packages/luvit/lit/v%LIT_VERSION%.zip"
