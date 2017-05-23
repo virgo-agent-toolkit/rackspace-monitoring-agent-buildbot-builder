d9ffdd160b9e6d2aedcda7be1af897a337d0660f
Ryan Phillips <ryan.phillips@rackspace.com>
2017-05-23 15:24:09 -0500

    bump lit to 3.5.4

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 echo off
 
 set LUVI_VERSION=release
-set LIT_VERSION=3.4.3
+set LIT_VERSION=3.5.4
 set RMA_VERSION=master
 
 set LIT_URL="https://lit.luvit.io/packages/luvit/lit/v%LIT_VERSION%.zip"
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LUVI_VERSION=rackspace
-LIT_VERSION=3.4.3
+LIT_VERSION=3.5.4
 RMA_VERSION=${RMA_VERSION:-master}
 
 LIT_URL="https://lit.luvit.io/packages/luvit/lit/v$LIT_VERSION.zip"
